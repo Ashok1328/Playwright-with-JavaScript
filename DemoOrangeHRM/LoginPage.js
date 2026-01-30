748d6c1de0090dd5363db7521a84e12a82043492
Sujit NEPAL <sujitnepal27@gmail.com>
2026-01-30 16:46:26 +0545

    feedback for login page

--- DemoOrangeHRM/LoginPage.js
+++ DemoOrangeHRM/LoginPage.js
@@ -1,20 +1,24 @@
 exports.LoginPage = class LoginPage {
   constructor(page) {
     this.page = page;
+
+     // Locators (defined once for reusability & readability) eg: page.locator(locator_value)
     this.userName = "input[name='username']",
     this.userPassword = "input[name='password']",
     this.submitBtn = "//button[@type='submit']";
   }
-
+    // naming conventions login lowercase do not user capitalcase
   async gotoLoginPage() {
+    // user navigateto and baseurl value from the playwright.config.js file
     await this.page.goto(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
     );
   }
-
+  // Login to login lowercase
   async Login(username, passsword) {
+    // use wait for visisbel element donot do hardcode playwright might not find the element due to slow network
     await this.page.locator(this.userName).fill(username);
-    await this.page.locator(this.userPassword).fill(passsword);
+    await this.page.locator(this.userPassword).fill(passsword); // typo error password 
     await this.page.locator(this.submitBtn).click();
   }
 };
