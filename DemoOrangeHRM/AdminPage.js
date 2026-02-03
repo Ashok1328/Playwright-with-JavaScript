0f5d17b7df1ad12d7de8a5b660aef20e63b7b822
Sujit NEPAL <sujitnepal27@gmail.com>
2026-02-03 10:24:04 +0545

    update the feedback code for the proper structure

--- DemoOrangeHRM/AdminPage.js
+++ DemoOrangeHRM/AdminPage.js
@@ -5,6 +5,10 @@ exports.AdminPage = class AdminPage {
     this.page = page;
 
     //Locators
+
+// const admin_locators = { admin_btn = 'xpath', and similarly all value of locator only with the variable name}
+
+
     this.AdminBtn = page.locator("//span[text()='Admin']");
     this.userNameInput = page.locator(
       "//form//input[contains(@class,'oxd-input')]",
@@ -29,18 +33,26 @@ exports.AdminPage = class AdminPage {
     this.searchBtn = page.locator("//button[@type='submit']");
   }
 
+// use the locator variable name as this.admin_btn = page.locator(admin_locator.admin_btn)
+
   async admin(username, employeename) {
     await expect(this.AdminBtn).toBeVisible();
     await this.AdminBtn.click();
     await expect(this.userNameInput).toBeEditable();
     await this.userNameInput.fill(username);
     await this.userRole.click();
+
+    // use await this.roleOption.click();
+    //await expect(this.userRole).toContainText("ESS"); do not use hard assertation.
     await expect(this.roleOption).toHaveText("ESS");
     await this.roleOption.click();
     await this.employeeName.fill(employeename);
+
+    // use tobevisible 
     await this.page.waitForSelector("//div[@role='option']", {
       state: "visible",
     });
+
     await this.employeeSelect.click();
     await expect(this.employeeName).toHaveValue("Peter Mac Anderson");
     await this.status.click();
